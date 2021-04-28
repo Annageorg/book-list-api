830a0d30e49f7bc658e85075b99afa24911e0cec
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-04-28 10:35:01 +0100

    comments on endpoints

--- router.js
+++ router.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 const booklist = require('./bookListController')
 
-/* GET home page*/
+// Get full booklisting
 router.get('/booklist', booklist.index)
+//Add books to list
 router.post('/booklist/create', booklist.create)
+//Show book by id
 router.get('/booklist/:id', booklist.show)
+//Remove book from listing by id
 router.delete('/booklist/:id', booklist.delete)
+//Edit book on the list by id
 router.put('/booklist/:id', booklist.update)
 
 module.exports = router
